211753ba1c7476bf3d078cfa8dad67fe176ae24c
Ryan DeVore <ryan@silverpine.com>
2021-11-10 20:55:02 -0800

    Make UUAssert methods public

--- core/src/main/java/com/silverpine/uu/test/core/UUAssert.java
+++ core/src/main/java/com/silverpine/uu/test/core/UUAssert.java
@@ -4,12 +4,12 @@ import org.junit.Assert;
 
 public class UUAssert
 {
-    static void assertSameNullness(Object lhs, Object rhs)
+    public static void assertSameNullness(Object lhs, Object rhs)
     {
         Assert.assertTrue(areSameNullness(lhs, rhs));
     }
 
-    static boolean areSameNullness(Object lhs, Object rhs)
+    public static boolean areSameNullness(Object lhs, Object rhs)
     {
         return ((lhs == null && rhs == null) || (lhs != null && rhs != null));
     }
